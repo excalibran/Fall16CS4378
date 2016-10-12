aaf178c3a52423214497010e9801c4d137a5680f
excalibran <excalibran@gmail.com>
2016-10-12 15:29:12 -0500

    a set of test sprites, script to adjust the camera, script to set instant movement

---
+++ Assets/scripts/PixelPerfectCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+    public static float pixelsToUnits = 1f;
+    public static float scale = 1f;
+
+    public Vector2 nativeRes = new Vector2(240, 160);
+
+    void Awake() {
+        var camera = GetComponent<Camera>();
+
+        if (camera.orthographic) {
+            scale = Screen.height / nativeRes.y;
+            pixelsToUnits *= scale;
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+        }
+    }
+}
