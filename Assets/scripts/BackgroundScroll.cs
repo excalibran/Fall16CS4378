53dad521424249c5914d8552377a2b8fd1eb1094
Jay <villegas.jason21@gmail.com>
2016-11-03 00:15:14 -0500

    Added moving background

---
+++ Assets/scripts/BackgroundScroll.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+		Material mat = mr.material;
+		Vector2 offset = mat.mainTextureOffset;
+		offset.y += Time.deltaTime / 10f;
+		mat.mainTextureOffset = offset;
+	}
+}
