25512ba52ae265a0a69db0a2071d0fc031358081
Jay <villegas.jason21@gmail.com>
2016-12-07 17:18:43 -0600

    new enemy, music, game over

---
+++ Assets/scripts/World/playExplosionAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playExplosionAudio : MonoBehaviour {
+	
+	public AudioSource explodeSource;
+	public AudioClip explodeClip;
+
+	// Use this for initialization
+	void Start () {
+		explodeSource.clip = explodeClip;
+		explodeSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
